0be547279448ac9c2ad48c1395a9057304367217
jian玄冰 <jianxuanhuo1@126.com>
2017-10-12 00:25:09 +0800

    领域对象实体

---
+++ src/JCE/Domains/Entities/IDomainObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JCE.Validations;
+
+namespace JCE.Domains.Entities
+{
+    /// <summary>
+    /// 领域对象
+    /// </summary>
+    public interface IDomainObject:IValidation
+    {
+    }
+}
