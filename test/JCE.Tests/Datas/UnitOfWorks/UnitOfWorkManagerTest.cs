5ba6f336df3e25f45f511c827ba3bde561e956f7
jian玄冰 <jianxuanhuo1@126.com>
2017-10-17 17:10:36 +0800

    加入MD5加密工具类

---
+++ test/JCE.Tests/Datas/UnitOfWorks/UnitOfWorkManagerTest.cs
@@ -0,0 +1,50 @@
+﻿using JCE.Datas.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JCE.Tests.XUnitHelpers;
+using NSubstitute;
+using Xunit;
+
+namespace JCE.Tests.Datas.UnitOfWorks
+{
+    /// <summary>
+    /// 工作单元管理器测试
+    /// </summary>
+    public class UnitOfWorkManagerTest
+    {
+        /// <summary>
+        /// 工作单元
+        /// </summary>
+        private readonly IUnitOfWork _unitOfWork;
+
+        /// <summary>
+        /// 工作单元2
+        /// </summary>
+        private readonly IUnitOfWork _unitOfWork2;
+
+        /// <summary>
+        /// 工作单元管理器
+        /// </summary>
+        private readonly UnitOfWorkManager _manager;
+
+        /// <summary>
+        /// 测试初始化
+        /// </summary>
+        public UnitOfWorkManagerTest()
+        {
+            _unitOfWork = Substitute.For<IUnitOfWork>();
+            _unitOfWork2 = Substitute.For<IUnitOfWork>();
+            _manager=new UnitOfWorkManager();
+        }
+
+        /// <summary>
+        /// 注册一个空工作单元
+        /// </summary>
+        [Fact]
+        public void TestRegister_Null()
+        {
+            AssertHelper.Throws<ArgumentNullException>(() => _manager.Register(null));
+        }
+    }
+}
---
+++ test/JCE.Tests/XUnitHelpers/AssertHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspectCore.DynamicProxy;
+using Xunit;
+
+namespace JCE.Tests.XUnitHelpers
+{
+    /// <summary>
+    /// 断言操作
+    /// </summary>
+    public class AssertHelper
+    {
+        /// <summary>
+        /// 抛出异常，并从异常消息中搜索特定关键字
+        /// </summary>
+        /// <typeparam name="TException">异常类型</typeparam>
+        /// <param name="action">操作</param>
+        /// <param name="keyword">关键字</param>
+        public static TException Throws<TException>(Action action, string keyword = "") where TException : Exception
+        {
+            var exception = GetException<TException>(action);
+            if (!string.IsNullOrWhiteSpace(keyword))
+                Assert.Contains(keyword, exception.Message);
+            return exception;
+        }
+
+        /// <summary>
+        /// 获取异常
+        /// </summary>
+        private static TException GetException<TException>(Action action) where TException : Exception
+        {
+            try
+            {
+                var exception = Assert.Throws<AspectInvocationException>(action);
+                return (TException)exception.InnerException;
+            }
+            catch
+            {
+                return Assert.Throws<TException>(action);
+            }
+        }
+    }
+}
