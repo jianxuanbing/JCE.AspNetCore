2e117573963577f4575c7b860923cc6891824e2d
jian玄冰 <jianxuanhuo1@126.com>
2017-11-01 11:50:34 +0800

    实现 哈希加密

---
+++ test/JCE.Utils.Encrypts.Test/Hash/HmacCryptorTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JCE.Utils.Encrypts.Hash;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JCE.Utils.Encrypts.Test.Hash
+{
+    public class HmacCryptorTest:TestBase
+    {
+        private const string Key = "123456";
+        private const string Source = "MD201711011120370001";
+        public HmacCryptorTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Test_HmacSha1()
+        {
+            var result = HmacCryptor.HmacSha1(Source, Key);
+            Output.WriteLine(result);
+            Assert.Equal("4d5b8eed4e4a8d4cdcdc78ef1537de34cdc4690f", result);
+        }
+
+        [Fact]
+        public void Test_HmacSha256()
+        {
+            var result = HmacCryptor.HmacSha256(Source, Key);
+            Output.WriteLine(result);
+            Assert.Equal("516548a67fc4d0094cdac8886cd372e015aa835291145714e77da11fd1d1c471", result);
+        }
+
+        [Fact]
+        public void Test_HmacSha384()
+        {
+            var result = HmacCryptor.HmacSha384(Source, Key);
+            Output.WriteLine(result);
+            Assert.Equal("204b2b899b5979c1c5ad17f65c3461786294a86888c29c0eeb2bebac13125a31e252334650e1c55664956b2b609a0d53", result);
+        }
+
+        [Fact]
+        public void Test_HmacSha512()
+        {
+            var result = HmacCryptor.HmacSha512(Source, Key);
+            Output.WriteLine(result);
+            Assert.Equal("194058ee50e9f24b42c6abdcc7fb79a008e458b960766f1aa2fd2df8043216c036c433948222269e38dc1bcaadd4fc16507e583f07fe0370216528571fe7d6e0", result);
+        }
+
+        [Fact]
+        public void Test_HmacMd5()
+        {
+            var result = HmacCryptor.HmacMd5(Source, Key);
+            Output.WriteLine(result);
+            Assert.Equal("1e538360d7a8ce373efc8721f6e7043d", result);
+        }
+    }
+}
---
+++ test/JCE.Utils.Encrypts.Test/Hash/SHACryptorTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JCE.Utils.Encrypts.Hash;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JCE.Utils.Encrypts.Test.Hash
+{
+    public class SHACryptorTest:TestBase
+    {        
+        private const string Source = "MD201711011120370001";
+        public SHACryptorTest(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Test_Sha1()
+        {
+            var result = SHACryptor.Sha1(Source);
+            Output.WriteLine(result);
+            Assert.Equal("29288e3dd3bc4e2de7676817df387164dde1af39",result);
+        }
+
+        [Fact]
+        public void Test_Sha256()
+        {
+            var result = SHACryptor.Sha256(Source);
+            Output.WriteLine(result);
+            Assert.Equal("bb359fa9532e95ec9e09283dac96943c17206c007f89afdf2c00fc7c41930a9e",result);
+        }
+
+        [Fact]
+        public void Test_Sha384()
+        {
+            var result = SHACryptor.Sha384(Source);
+            Output.WriteLine(result);
+            Assert.Equal("551d245291a875146cbcd640a0fbc8d9eaf447cd4cba272834444a4f401f436dad7654035e473dd82389382ddb646d61", result);
+        }
+
+        [Fact]
+        public void Test_Sha512()
+        {
+            var result = SHACryptor.Sha512(Source);
+            Output.WriteLine(result);
+            Assert.Equal("29443332fe291c69edbce0b9060f0ddd4c912009772a468d52731f4b41867eb666b6bc9ff766180a06352061643abd666b9c6bdbad302c230aac68668b98eaf9", result);
+        }
+    }
+}
