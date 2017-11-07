1036bf809ddd6dbc01a6487eca3f42fcd55c28b7
jian玄冰 <jianxuanhuo1@126.com>
2017-11-07 18:01:29 +0800

    加入中文测试

--- test/JCE.Tools.QrCode.ZXing.Test/UnitTest1.cs
+++ test/JCE.Tools.QrCode.ZXing.Test/UnitTest1.cs
@@ -30,8 +30,22 @@ namespace JCE.Tools.QrCode.ZXing.Test
             {
                 var fileName = service.Save("http://www.cnblogs.com/jianxuanbing/p/7376757.html");
                 Output.WriteLine(fileName);
+            }            
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var factory = new QrCodeFactory();
+            var service = factory.Create("D:\\Test");
+            service.Correction(ErrorCorrectionLevel.M);
+            service.Size(QrSize.Large);
+            service.Logo(@"D:\Test\test.jpg");
+            for (int i = 0; i < 1; i++)
+            {
+                var fileName = service.Save("这是一个非常严重的Bug，因此我需要进行验证一下中文的情况下是否能生成二维码");
+                Output.WriteLine(fileName);
             }
-            
         }
 
         public UnitTest1(ITestOutputHelper output) : base(output)
